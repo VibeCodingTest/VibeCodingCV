819286552a0ac2e0f80e3871fad7d520284a9100
Lan Mena <lan4mena@gmail.com>
2025-11-11 20:40:16 -0600

    Ajustes para despliegue

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VibeCodingCV/',
-  server: {
-    host: true
-  }
+  base: '/VibeCodingCV/'
 });
